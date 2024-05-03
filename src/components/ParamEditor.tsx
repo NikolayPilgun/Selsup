ddd3ed2a39d1c23a3683a438186492b0d6cac7b5
Nikolay Pilgun <a03198814s@gmail.com>
2024-05-03 23:47:59 +0500

    Bug fixation

--- src/components/ParamEditor.tsx
+++ src/components/ParamEditor.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Model, Props } from "../types/ParamTypes";
 
 const ParamEditor: React.FC<Props> = ({ params, model }) => {
@@ -7,43 +7,40 @@ const ParamEditor: React.FC<Props> = ({ params, model }) => {
 	);
 
 	const updateParamValue = (paramId: number, value: string) => {
-		setEditedParams(
-			(prevEditedParams) => new Map(prevEditedParams.set(paramId, value))
-		);
+		setEditedParams((prevEditedParams) => {
+			const updatedParams = new Map(prevEditedParams);
+			updatedParams.set(paramId, value);
+			return updatedParams;
+		});
 	};
 
 	const getModel = (): Model => {
 		const updatedParamValues = model.paramValues.map((paramValue) => ({
 			...paramValue,
-			value: editedParams.get(paramValue.paramId) || paramValue.value,
+			value: editedParams.get(paramValue.paramId) ?? paramValue.value,
 		}));
 		return { paramValues: updatedParamValues };
 	};
 
 	return (
 		<article className="flex flex-col gap-5 justify-center items-center">
-			{params != null ? (
-				params.map((param) => (
-					<div className="w-[350px] " key={param.id}>
-						<label className="block text-gray-700 font-bold mb-2">
-							{param.name}
-						</label>
-						<input
-							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-md"
-							type="text"
-							value={
-								editedParams.get(param.id) ||
-								model.paramValues.find((pv) => pv.paramId === param.id)
-									?.value ||
-								""
-							}
-							onChange={(e) => updateParamValue(param.id, e.target.value)}
-						/>
-					</div>
-				))
-			) : (
-				<div>Загрузка...</div>
-			)}
+			{params.map((param) => (
+				<div className="w-[350px]" key={param.id}>
+					<label className="block text-gray-700 font-bold mb-2">
+						{param.name}
+					</label>
+					<input
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-md"
+						type="text"
+						value={
+							editedParams.get(param.id) ??
+							(model.paramValues.find((pv) => pv.paramId === param.id)?.value ||
+								"")
+						}
+						onChange={(e) => updateParamValue(param.id, e.target.value)}
+					/>
+				</div>
+			))}
 
 			<button
 				onClick={() => console.log(getModel())}
