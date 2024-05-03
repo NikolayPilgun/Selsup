65cc41b881d829c067238e34c60dc1072a1da0cf
Nikolay Pilgun <a03198814s@gmail.com>
2024-05-03 23:31:02 +0500

    The following changes have been made
     * improvement of asynchronous requests
     * download status
     * error status

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,39 @@ import ParamEditor from "./components/ParamEditor";
 import { Model, Param } from "./types/ParamTypes";
 import { fetchData } from "./utilities/asyncRequest";
 
+const API_URL_PARAMS = "http://localhost:3003/params";
+const API_URL_MODEL = "http://localhost:3004/model";
+
 const App: React.FC = () => {
 	const [dataParams, setDataParams] = useState<Param[] | null>(null);
 	const [dataModel, setDataModel] = useState<Model | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
 		const fetchDataFromBackend = async () => {
 			try {
-				const result = await fetchData<Param[]>("http://localhost:3003/params");
-				setDataParams(result);
+				const [paramsResult, modelResult] = await Promise.all([
+					fetchData<Param[]>(API_URL_PARAMS),
+					fetchData<Model>(API_URL_MODEL),
+				]);
+				setDataParams(paramsResult);
+				setDataModel(modelResult);
 			} catch (error) {
+				setError("Failed to fetch data from the server.");
 				console.error(error);
+			} finally {
+				setLoading(false);
 			}
 		};
 
 		fetchDataFromBackend();
 	}, []);
 
-	useEffect(() => {
-		const fetchDataFromBackend = async () => {
-			try {
-				const result = await fetchData<Model>("http://localhost:3004/model");
-				setDataModel(result);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-
-		fetchDataFromBackend();
-	}, []);
+	const paramEditorComponent = !loading &&
+		!error &&
+		dataParams &&
+		dataModel && <ParamEditor params={dataParams} model={dataModel} />;
 
 	return (
 		<>
@@ -42,10 +46,12 @@ const App: React.FC = () => {
 				<h2 className="text-xl text-center">Редактор параметров</h2>
 			</header>
 			<main className="flex justify-around items-center flex-wrap gap-8">
-				{dataParams != null && dataModel != null ? (
-					<ParamEditor params={dataParams} model={dataModel} />
-				) : (
+				{loading ? (
 					<div>Загрузка...</div>
+				) : error ? (
+					<div>{error}</div>
+				) : (
+					paramEditorComponent
 				)}
 			</main>
 		</>
